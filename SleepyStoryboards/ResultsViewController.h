d0623a50fb74ba28e83f74972ace5bf9bd5f5b67
ajfigueroa <alexjfigueroa@gmail.com>
2014-01-02 11:24:01 -0500

    Changed some semantics in UIColors (Colors) category. Adding results view controller implementation.

---
+++ SleepyStoryboards/ResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultsViewController.h
+//  SleepyStoryboards
+//
+//  Created by Alexander Figueroa on 1/2/2014.
+//  Copyright (c) 2014 Alexander Figueroa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultsViewController : UIViewController
+
+@end
---
+++ SleepyStoryboards/ResultsViewController.m
@@ -0,0 +1,38 @@
+//
+//  ResultsViewController.m
+//  SleepyStoryboards
+//
+//  Created by Alexander Figueroa on 1/2/2014.
+//  Copyright (c) 2014 Alexander Figueroa. All rights reserved.
+//
+
+#import "ResultsViewController.h"
+
+@interface ResultsViewController ()
+
+@end
+
+@implementation ResultsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
