0b3c80a30b54ad98ffd36cb4fe6e6cd6d8747e10
ajfigueroa <alexjfigueroa@gmail.com>
2014-01-24 15:05:54 -0500

    Beinning implementation of Singleton SettingsAPI

---
+++ SleepyStoryboards/SettingsAPI.m
@@ -0,0 +1,28 @@
+//
+//  SettingsAPI.m
+//  SleepyStoryboards
+//
+//  Created by Alexander Figueroa on 1/24/2014.
+//  Copyright (c) 2014 Alexander Figueroa. All rights reserved.
+//
+
+#import "SettingsAPI.h"
+
+@implementation SettingsAPI
+
++ (instancetype)sharedSettingsAPI
+{
+    static SettingsAPI *sharedSettingsAPI = nil;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedSettingsAPI = [[SettingsAPI alloc] init];
+    });
+    
+    return sharedSettingsAPI;
+}
+
+
+
+
+@end
