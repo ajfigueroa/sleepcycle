f4f0238f316dd604c58618fff2bdad4f4455d387
ajfigueroa <alexjfigueroa@gmail.com>
2014-01-26 22:01:11 -0500

    Added singleton declaration in SchedulerAPI and init method. Added empty stubs for public methods

--- SleepyStoryboards/SchedulerAPI.m
+++ SleepyStoryboards/SchedulerAPI.m
@@ -8,13 +8,50 @@
 //  Responsible for setting up all notifications (alarms) and reminders
 
 #import "SchedulerAPI.h"
+#import "ReminderScheduler.h"
 
-@interface SchedulerAPI ()
+@interface SchedulerAPI () <ReminderSchedulerDelegate>
+
+@property (nonatomic, strong) ReminderScheduler *reminderScheduler;
 
 @end
 
 @implementation SchedulerAPI
 
++ (instancetype)sharedScheduler
+{
+    static SchedulerAPI *sharedScheduler = nil;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedScheduler = [[SchedulerAPI alloc] init];
+    });
+    
+    return sharedScheduler;
+}
 
+- (instancetype)init
+{
+    self = [super init];
+    
+    if (self)
+    {
+        self.reminderScheduler = [[ReminderScheduler alloc] init];
+        self.reminderScheduler.delegate = (id <ReminderSchedulerDelegate>)self;
+    }
+    
+    return self;
+}
+
+#pragma mark - Public Scheduling Methods
+- (void)createAlarmNotificationForDate:(NSDate *)alarmDate
+{
+    
+}
+
+- (void)createReminderForDate:(NSDate *)reminderDate
+{
+    
+}
 
 @end
