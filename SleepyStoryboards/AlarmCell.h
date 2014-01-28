7ac5f06eb2fd169511e4114bf1673607c89309e6
ajfigueroa <alexjfigueroa@gmail.com>
2014-01-28 00:28:19 -0500

    Added and hooked up AlarmCell outlets

---
+++ SleepyStoryboards/AlarmCell.h
@@ -0,0 +1,16 @@
+//
+//  AlarmCell.h
+//  SleepyStoryboards
+//
+//  Created by Alexander Figueroa on 1/27/2014.
+//  Copyright (c) 2014 Alexander Figueroa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlarmCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *alarmTimeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *alarmDateLabel;
+
+@end
---
+++ SleepyStoryboards/AlarmCell.m
@@ -0,0 +1,29 @@
+//
+//  AlarmCell.m
+//  SleepyStoryboards
+//
+//  Created by Alexander Figueroa on 1/27/2014.
+//  Copyright (c) 2014 Alexander Figueroa. All rights reserved.
+//
+
+#import "AlarmCell.h"
+
+@implementation AlarmCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
