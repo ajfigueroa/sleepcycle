81982dbb4d8d1bf20075f33405d7336efde41fef
ajfigueroa <alexjfigueroa@gmail.com>
2014-01-31 11:23:04 -0500

    Added documentation for ImageViewManagerFactory

--- SleepyStoryboards/ImageViewManagerFactory.h
+++ SleepyStoryboards/ImageViewManagerFactory.h
@@ -11,7 +11,18 @@
 
 @interface ImageViewManagerFactory : NSObject
 
+/**
+ @brief Returns the shared factory for creating Image View Manager objects.
+ @returns The shared image view manager factory.
+ */
 + (instancetype)sharedImageViewManagerFactory;
+
+/**
+ @brief Asks the sharedImageViewManagerFactory to build an ImageViewManager object based on the themeKey constant value which corresponds to a Theme conforming object.
+ @param themeKey The AFThemeSelectionOption constant that corresponds to a given theme.
+ @sa ThemeFactory.h
+ @returns An object that conforms to the ImageViewManager protocol that is responsible for providing images based on the Theme. (Dark themes are better suited to light overlay images, whereas light themes would be better suited to dark overlay images).  Returns @b nil, if the themeKey is not valid.
+ */
 - (id <ImageViewManager>)buildImageViewManagerForThemeKey:(AFThemeSelectionOption)themeKey;
 
 @end
