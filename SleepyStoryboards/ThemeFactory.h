90aaf8362c14fed1d0fac38f41e9a8ad911bc8ef
ajfigueroa <alexjfigueroa@gmail.com>
2014-01-31 11:07:09 -0500

    Added documentation for ThemeFactory class

--- SleepyStoryboards/ThemeFactory.h
+++ SleepyStoryboards/ThemeFactory.h
@@ -13,9 +13,26 @@
 
 @interface ThemeFactory : NSObject
 
+/**
+ @brief Returns the current application's shared theme factory
+ @sa Theme protocol in Theme.h
+ @returns The current application's shared theme factory
+ */
 + (instancetype)sharedThemeFactory;
 
+/**
+ @brief Asks the sharedThemeFactory to build a Theme object that corresponds to
+ the themeKey constant provided.
+ @param themeKey The AFThemeSelectionOption constant that corresponds to the desired theme.
+ @sa AFThemeSelectionOption constants in SleepyTimeConstants.h
+ @returns An object that conforms to the Theme protocol to be used for theming UIControls and UIViews. Returns nil if themeKey is not valid.
+ */
 - (id <Theme>)buildThemeForKey:(AFThemeSelectionOption)themeKey;
+
+/**
+ @brief Asks the sharedThemeFactory to build a Theme object based off the persistent themeKey constant in NSUserDefaults.
+ @returns An object that conforms to the Theme protocol to be used for theming UIControls and UIViews. Returns nil if the provided themeKey is not valid since this method calls: -[ThemeFactory buildThemeForKey:themeKey];
+ */
 - (id <Theme>)buildThemeForSettingsKey;
 
 @end
