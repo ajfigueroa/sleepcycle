4b410f0d17c7b37e712f7308e7e7896883086931
ajfigueroa <alexjfigueroa@gmail.com>
2014-01-31 11:12:52 -0500

    Added documentation for ImageViewManager.h

--- SleepyStoryboards/ImageViewManager.h
+++ SleepyStoryboards/ImageViewManager.h
@@ -10,9 +10,28 @@
 
 @protocol ImageViewManager <NSObject>
 
+/**
+ @brief Returns the image to be used for the Settings cell
+ @returns The UIImage to be used in the Settings cell's imageView.
+ */
 - (UIImage *)settingsImage;
+
+/**
+ @brief Returns the image to be used for the Bed Time cell.
+ @returns The UIImage to be used in the Bed Time cell's imageView.
+ */
 - (UIImage *)bedTimeImage;
+
+/**
+ @brief Returns the image to be used for the Wake Time cell.
+ @returns The UIImage to be used in the Wake Time cell's imageView.
+ */
 - (UIImage *)wakeTimeImage;
+
+/**
+ @brief Returns the image to be used for the Alarm cell.
+ @returns The UIImage to be used in the Alarm cell's imageView.
+ */
 - (UIImage *)alarmImage;
 
 @end
