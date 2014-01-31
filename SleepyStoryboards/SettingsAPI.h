5b2f2d742bff699a6cb353fad6143d5628dc33cb
ajfigueroa <alexjfigueroa@gmail.com>
2014-01-31 12:53:25 -0500

    Documentation for SettingsAPI

--- SleepyStoryboards/SettingsAPI.h
+++ SleepyStoryboards/SettingsAPI.h
@@ -12,17 +12,52 @@
 
 @interface SettingsAPI : NSObject
 
-// Interface for Settings properties of the same name
+/**
+ @brief The time to fall asleep in minutes.
+ @note This is part of the SettingsAPI interface.
+ */
 @property (nonatomic, assign) NSInteger timeToFallAsleep;
+
+/**
+ @brief The enumeration that corresponds to the applications current theme.
+ @note This is part of the SettingsAPI interface.
+ */
 @property (nonatomic, assign) NSInteger appTheme;
+
+/**
+ @brief The bool that controls whether or not the border around the main UIDatePicker in the TimeSelectionViewController is visibile.
+ @note This is part of the SettingsAPI interface.
+ */
 @property (nonatomic, assign) BOOL showBorder;
+
+/**
+ @brief The bool that controls whether the UITableView in ResultsViewController has the Ping-Pong slide to
+ refresh control.
+ @note This is part of the SettingsAPI interface.
+ */
 @property (nonatomic, assign) BOOL showEasterEgg;
 
+/**
+ @brief The NSString representation of the appTheme's name. This is used for formatting to the views.
+ */
+@property (nonatomic, assign) NSString *appThemeName;
+
+/**
+ @brief Returns the shared Settings API object.
+ @returns The shared SettingsAPI object to manage Persistency and Theme formatting.
+ */
 + (instancetype)sharedSettingsAPI;
 
-- (NSString *)appThemeName;
-- (void)setAppThemeName:(NSString *)newAppThemeName;
+/**
+ @brief The array of all the available theme names based off the AFThemeSelectionOption enumerations.
+ @returns A NSArray of available theme names as strings.
+ */
 - (NSArray *)themeNames;
+
+/**
+ @brief Informs the SettingsAPI persistency manager to save all states.
+ @note You should call this @b only when you are ready to save.
+ */
 - (void)saveAllSettings;
 
 @end
