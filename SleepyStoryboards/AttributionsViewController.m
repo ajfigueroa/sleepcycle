680fe3578cf15cae45708264fefe56ce1cd5990c
ajfigueroa <alexjfigueroa@gmail.com>
2014-02-08 03:57:35 -0500

    Completed contributions list and added more constraints to the UITextView in the AttributionsViewController

--- SleepyStoryboards/AttributionsViewController.m
+++ SleepyStoryboards/AttributionsViewController.m
@@ -25,13 +25,35 @@
 
 - (void)buildAttributionsText
 {
-    NSString *text = @"\nFlatUIKit\n"
-                     @"https://github.com/grouper/flatuikit\n";
+    NSString *text = @"\nLIBRARIES\n\n"
+                     @"FlatUIKit\n"
+                     @"https://github.com/grouper/flatuikit\n\n"
+                     @"IBActionSheet\n"
+                     @"https://github.com/ianb821/IBActionSheet\n\n"
+                     @"SVProgressHUD\n"
+                     @"https://github.com/samvermette/SVProgressHUD\n\n"
+                     @"JSSlidingViewController\n"
+                     @"https://github.com/jaredsinclair/JSSlidingViewController\n\n"
+                     @"BOZPongRefreshControl\n"
+                     @"https://github.com/boztalay/BOZPongRefreshControl\n\n"
+                     @"Colors-for-iOS\n"
+                     @"https://github.com/bennyguitar/Colours\n\n"
+                     @"------------------------------------------------------------"
+                     @"\n\nICONS\n\n"
+                     @"Moon, Sun, Clock, Settings, and List\n"
+                     @"http://somerandomdude.com/work/iconic/\n\n"
+                     @"------------------------------------------------------------"
+                     @"\n\nSOUNDS\n\n"
+                     @"Alarm Sound was made by Joe DeShon\n"
+                     @"http://www.freesound.org/people/joedeshon/\n\n"
+                     @"------------------------------------------------------------"
+                     @"\n\nICONS and SOUNDS under Creative Commons License\n"
+                     @"http://creativecommons.org/licenses/by/3.0/\n\n";
     
     self.attributionTextView.text = NSLocalizedString(text, nil);
     self.attributionTextView.textAlignment = NSTextAlignmentCenter;
     self.attributionTextView.font = [UIFont fontWithName:@"Futura" size:[UIFont labelFontSize]];
-    self.attributionTextView.textContainerInset = UIEdgeInsetsMake(5.0f, 5.0f, 5.0f, 5.0f);
+    self.attributionTextView.textContainerInset = UIEdgeInsetsZero;
     self.attributionTextView.editable = NO;
     self.attributionTextView.dataDetectorTypes = UIDataDetectorTypeAll;
 }
