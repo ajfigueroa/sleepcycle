95c5729429b0ffc661ed7254b9913f1c6f8888b0
ajfigueroa <alexjfigueroa@gmail.com>
2014-02-21 17:38:52 -0500

    Adding linkTitles and UITableViewDataSource methods to the AboutViewController

--- SleepyStoryboards/Classes/AboutViewController.m
+++ SleepyStoryboards/Classes/AboutViewController.m
@@ -8,11 +8,50 @@
 
 #import "AboutViewController.h"
 
-@interface AboutViewController ()
+@interface AboutViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextView *aboutTextView;
+
+@property (nonatomic, strong) UITableView *tableView;
+
+@property (nonatomic, strong) NSArray *linkTitles;
 
 @end
 
 @implementation AboutViewController
 
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self buildAboutText];
+    [self buildLinkTitles];
+}
+
+- (void)buildAboutText
+{
+    NSString *text = @"SleepCycle was inspired by the website sleepyti.me, feel free to check it out.\n\n"
+                     @"This project is also open source and if you would like to contribute or use anything from "
+                     @"within this application in yours, feel free to.\n\n"
+                     @"If you have any questions or critiques about my design choices, feel free to get in touch with "
+                     @"me as I still have a lot to learn.";
+    
+    self.aboutTextView.text = NSLocalizedString(text, nil);
+    self.aboutTextView.textAlignment = NSTextAlignmentCenter;
+    self.aboutTextView.font = [UIFont fontWithName:@"Futura" size:14.0f];
+    self.aboutTextView.textContainerInset = UIEdgeInsetsMake(10.0f, 4.0f, 10.0f, 4.0f);
+    self.aboutTextView.editable = NO;
+}
+
+- (void)buildLinkTitles
+{
+    
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.linkTitles.count;
+}
 
 @end
