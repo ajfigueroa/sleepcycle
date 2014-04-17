29d9169bd6cb9e31da41737459c7edaa2bd3ab58
ajfigueroa <alexjfigueroa@gmail.com>
2014-04-17 19:48:46 -0400

    Adding dictionary key for Defaults.plist into AFUserDefaultKeyConstants

--- SleepyStoryboards/Constants/AFUserDefaultKeyConstants.h
+++ SleepyStoryboards/Constants/AFUserDefaultKeyConstants.h
@@ -20,4 +20,6 @@ static NSString *const AFTimeToFallAsleepInMinutes = @"TimeToFallAsleepInMinutes
 
 static NSString *const AFAttributionsDictionary = @"AttributionsDictionary";
 
+static NSString *const AFShowInfoAtLaunch = @"ShowInfoAtLaunch";
+
 #endif
